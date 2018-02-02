92996991fd872bc77ef519c177866a3bb168b874
fluff <34669874+fluffdotexe@users.noreply.github.com>
2018-02-01 23:41:55 -0500

    Test changes to accommodate modal login form. Hide the Sign up now! button on login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
-    assert_response :success
+    assert_response :found
   end
 
 end
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -5,25 +5,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post users_path, xhr: true, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    assert_template 'users/new'
+    assert_template 'shared/_error_messages'
+    assert_template 'layouts/_signup_form'
   end
   
   
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
+      post users_path, xhr: true, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
+    assert_template 'users/create'
     assert is_logged_in?
   end
   
