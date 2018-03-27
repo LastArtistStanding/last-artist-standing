39d04cf7250e91bd036bf4fc21ef10bff84c6c46
the1banana <the1banana@users.noreply.github.com>
2018-03-27 16:15:39 -0500

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "DAD.GALLERY | Password reset"
+    mail to: user.email, subject: "Do Art Daily - Password Reset"
   end
 end
