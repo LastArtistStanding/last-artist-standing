4c5f78220474b02e25224c86f50abb87bf29984e
shawsey <ntshawsey@gmail.com>
2018-03-26 22:20:22 -1000

    Revert "Password reset"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token, :reset_token
+    
     has_many :awards
     has_many :submissions
     has_many :participations
@@ -23,26 +23,6 @@ class User < ActiveRecord::Base
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
       end
-      
-  # Sets the password reset attributes.
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-  
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-  
-  def password_reset_expired?
-      reset_sent_at < 2.hours.ago
-  end
 end
 
 
