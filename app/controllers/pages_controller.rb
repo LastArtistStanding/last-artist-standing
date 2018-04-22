33c329e58330c6e1862e7e26641db18193ec55c3
the1banana <the1banana@users.noreply.github.com>
2018-04-22 11:13:04 -0500

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
         
         # All lists to be displayed in home
         @latestAwards = Award.where("date_received = ?", Date.today).order("badge_id DESC, prestige DESC")
-        @latestEliminations = Participation.where("challenge_id = 1 AND eliminated AND end_date = ?", Date.current).order("score DESC")
+        @latestEliminations = Participation.where("challenge_id = 1 AND eliminated AND end_date = ?", (Date.current - 1.day)).order("score DESC")
         @startingChallenges = @activeChallenges.where('start_date = ?', Date.current)
         @endingChallenges = Challenge.where('end_date = ?', Date.current)
         @seasonalLeaderboard = Participation.where("challenge_id = ?", currentSeasonalChallenge).order("score DESC")
