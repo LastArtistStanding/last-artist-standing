4f7697afbeaffac926345db5f6f2c3b0236bac12
the1banana <the1banana@users.noreply.github.com>
2018-12-31 18:12:01 -0600

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [134.9, 134.9]
+    process :resize_to_fill => [135, 135]
   end
   
   version :avatar do
