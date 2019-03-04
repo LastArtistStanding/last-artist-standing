ad3fe90cba632a5eef406c65b2290928e5b75d11
EC2 Default User <ec2-user@ip-172-31-46-98.us-east-2.compute.internal>
2019-03-04 04:08:50 +0000

    Add level-based restrictions for spam prevention.

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,82 +1,81 @@
 module ChallengesHelper
+  def getChallengeCreator(id)
+    if id.present?
+      if id == -1
+        "User Account Deleted"
+      else
+        link_to(User.find_by(id: id).name, User.find_by(id: id))
+      end
+    else
+      "Site Challenge"
+    end
+  end
     
-    def getChallengeCreator(id)
-        if id.present?
-            if id == -1
-                "User Account Deleted"
-            else
-                link_to(User.find_by(id: id).name, User.find_by(id: id))
-            end
-        else
-            "Site Challenge"
-        end
+  def userIsInChallenge(user, challenge)
+    !Participation.find_by(:user_id => user.id, :challenge_id => challenge.id).blank?
+  end
+    
+  def getDADChallenge
+    Challenge.find(1)
+  end
+    
+  def getCurrentSeasonalChallenge
+    Challenge.where(":todays_date >= start_date AND :todays_date < end_date AND seasonal = true", {todays_date: Date.current}).first
+  end
+    
+  def getDaysInChallenge(challenge)
+    if challenge.blank?
+      0
+    else
+      (challenge.end_date - challenge.start_date).to_i
+    end
+  end
+    
+  def getBadgeThreshold(challenge)
+    BadgeMap.where(:challenge_id => challenge.id).order("required_score ASC").first.required_score
+  end
+    
+  def getParticipationLevel(participation, challenge)
+    if participation.blank?
+      0
+    elsif participation.score >= getBadgeThreshold(challenge)
+      2
+    else
+      1
+    end
+  end
+    
+  def getUserParticipationLevel(user, challenge, active)
+    return 0 if user.nil?
+  
+    if active
+      participation = Participation.find_by(:user_id => user.id, :challenge_id => challenge.id, :active => true)
+    else
+      participation = Participation.find_by(:user_id => user.id, :challenge_id => challenge.id)
     end
     
-    def userIsInChallenge(user, challenge)
-        !Participation.find_by(:user_id => user.id, :challenge_id => challenge.id).blank?
-    end
+    getParticipationLevel(participation, challenge)
+  end
     
-    def getDADChallenge
-        Challenge.find(1)
+  def getParticipationIcon(value)
+    case value
+    when 0
+      "<i class='fa fa-times-circle' aria-hidden='true'></i>"
+    when 1
+      "<i class='fa fa-check' aria-hidden='true'></i>"
+    when 2
+      "<i class='fa fa-trophy' aria-hidden='true'></i>"
     end
+  end
     
-    def getCurrentSeasonalChallenge
-        Challenge.where(":todays_date >= start_date AND :todays_date < end_date AND seasonal = true", {todays_date: Date.current}).first
+  def getAwardByUserAndChallenge(challenge, user)
+    if challenge.blank?
+      nil
+    elsif user.blank?
+      nil
+    else
+      badge_map = BadgeMap.find_by(challenge_id: challenge.id)
+      Award.find_by(:user_id => user.id, :badge_id => badge_map.badge_id)
     end
-    
-    def getDaysInChallenge(challenge)
-        if challenge.blank?
-            0
-        else
-            (challenge.end_date - challenge.start_date).to_i
-        end
-    end
-    
-    def getBadgeThreshold(challenge)
-        BadgeMap.where(:challenge_id => challenge.id).order("required_score ASC").first.required_score
-    end
-    
-    def getParticipationLevel(participation, challenge)
-        if participation.blank?
-            0
-        elsif participation.score >= getBadgeThreshold(challenge)
-            2
-        else
-            1
-        end
-    end
-    
-    def getUserParticipationLevel(user, challenge, active)
-        return 0 if user.nil?
-        
-        if active
-            participation = Participation.find_by(:user_id => user.id, :challenge_id => challenge.id, :active => true)
-        else
-            participation = Participation.find_by(:user_id => user.id, :challenge_id => challenge.id)
-        end
-        getParticipationLevel(participation, challenge)
-    end
-    
-    def getParticipationIcon(value)
-        case value
-        when 0
-            "<i class='fa fa-times-circle' aria-hidden='true'></i>"
-        when 1
-            "<i class='fa fa-check' aria-hidden='true'></i>"
-        when 2
-            "<i class='fa fa-trophy' aria-hidden='true'></i>"
-        end
-    end
-    
-    def getAwardByUserAndChallenge(challenge, user)
-        if challenge.blank?
-            nil
-        elsif user.blank?
-            nil
-        else
-            badge_map = BadgeMap.find_by(challenge_id: challenge.id)
-            Award.find_by(:user_id => user.id, :badge_id => badge_map.badge_id)
-        end
-    end
-    
+  end
 end
--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,32 +1,22 @@
 module SubmissionsHelper
-
-    def getNsfwString(level)
-      case level
-      when 1
-        "Safe"
-      when 2
-        "Questionable"
-      when 3
-        "Explicit"
-      else
-        "???"
-      end
+  def getNsfwString(level)
+    case level
+    when 1
+      "Safe"
+    when 2
+      "Questionable"
+    when 3
+      "Explicit"
+    else
+      "???"
     end
+  end
     
-    def getSubmissionThumb(submission)
-      if submission.blank?
-        ""
-      elsif !logged_in? || current_user.nsfw_level.blank?
-        if submission.nsfw_level > 1
-          "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Thumb.png"
-        else
-          if submission.drawing.blank?
-            ""
-          else
-            submission.drawing.thumb.url
-          end
-        end
-      elsif current_user.nsfw_level < submission.nsfw_level
+  def getSubmissionThumb(submission)
+    if submission.blank?
+      ""
+    elsif !logged_in? || current_user.nsfw_level.blank?
+      if submission.nsfw_level > 1
         "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Thumb.png"
       else
         if submission.drawing.blank?
@@ -35,20 +25,20 @@ module SubmissionsHelper
           submission.drawing.thumb.url
         end
       end
+    elsif current_user.nsfw_level < submission.nsfw_level
+      "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Thumb.png"
+    else
+      if submission.drawing.blank?
+        ""
+      else
+        submission.drawing.thumb.url
+      end
     end
+  end
     
-    def getSubmissionAvatar(submission)
-      if !logged_in? || current_user.nsfw_level.blank?
-        if submission.nsfw_level > 1
-          "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Avatar.png"
-        else
-          if submission.drawing.blank?
-            ""
-          else
-            submission.drawing.avatar.url
-          end
-        end
-      elsif current_user.nsfw_level < submission.nsfw_level
+  def getSubmissionAvatar(submission)
+    if !logged_in? || current_user.nsfw_level.blank?
+      if submission.nsfw_level > 1
         "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Avatar.png"
       else
         if submission.drawing.blank?
@@ -57,7 +47,16 @@ module SubmissionsHelper
           submission.drawing.avatar.url
         end
       end
+    elsif current_user.nsfw_level < submission.nsfw_level
+      "https://s3.us-east-2.amazonaws.com/do-art-daily-public/Content+Filter+Avatar.png"
+    else
+      if submission.drawing.blank?
+        ""
+      else
+        submission.drawing.avatar.url
+      end
     end
+  end
     
   def getSubmissionTimeText(submission)
     if submission.time.blank?
@@ -80,18 +79,6 @@ module SubmissionsHelper
       "No description provided."
     else
       submission.description
-    end
-  end
-    
-  def can_comment_on_submission(submission, user)
-    return [false, "The submitter has closed comments on this submission."] unless submission.commentable
-    
-    if !logged_in?
-      [false, "You must log in to post comments."]
-    elsif getUserMaxLevel(user) < 3
-      [false, "You need to have reached DAD level 3 (11 submission streak) to comment on submissions."]
-    else
-      [true, nil]
     end
   end
 end
