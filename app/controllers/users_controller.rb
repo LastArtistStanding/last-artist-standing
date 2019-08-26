c01455e727c83cee1d4df4c7d55013a17c7e630a
EC2 Default User <ec2-user@ip-172-31-42-212.us-east-2.compute.internal>
2019-08-26 04:03:08 +0000

    display_name searchable, better sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     #@users = User.order("id DESC").paginate(:page => params[:page], :per_page => 50)
-    @users = User.search(params).paginate(:page => params[:page], :per_page => 25 ).order("id DESC")
+    @users = User.search(params).paginate(:page => params[:page], :per_page => 25 ).order("current_streak DESC, id DESC")
   end
   
   def submissions
