8e016d1424a0049ba320ec04da20c4752c741f1a
the1banana <the1banana@users.noreply.github.com>
2019-10-21 23:23:02 -0500

    Revert "User experience bonuses."
    This reverts commit 91f88ee315e1f025782be8c5a78da94e216bd37d.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,45 +1,24 @@
 class PagesController < ApplicationController
     
     def home
-      @activeChallenges = Challenge.where('start_date <= ? AND (end_date > ? OR end_date IS NULL)', Date.current, Date.current).order("start_date ASC, end_date DESC")
-      @upcomingChallenges = Challenge.where('start_date > ?', Date.current).order("start_date ASC, end_date DESC")
-      @currentSeasonalChallenge = Challenge.where(":todays_date >= start_date AND :todays_date < end_date AND seasonal = true", {todays_date: Date.current}).first
-      
-      # All lists to be displayed in home
-      @latestAwards = Award.where("date_received = ? AND badge_id <> 1", Date.today).order("prestige DESC, badge_id DESC").includes(:user)
-      @levelUps = Award.where("date_received = ? AND badge_id = 1", Date.today).order("prestige DESC").includes(:user)
-      @latestEliminations = Participation.where("challenge_id = 1 AND eliminated AND end_date = ?", (Date.current - 1.day)).order("score DESC").includes(:user)
-      @startingChallenges = @activeChallenges.where('start_date = ?', Date.current)
-      @endingChallenges = Challenge.where('end_date = ?', Date.current)
-      @seasonalLeaderboard = Participation.where("challenge_id = ?", @currentSeasonalChallenge.id).order("score DESC").includes(:user)
-      
-      @activity = []
-      challenge_activity = Challenge.where('creator_id > 0').order("created_at DESC").limit(10)
-      challenge_activity.each do |c|
-        activity_hash = { message: "Challenge '#{c.name}' was created.", datetime: c.created_at, link: challenge_path(c.id) }
-        @activity.push(activity_hash)
-      end
-      submission_activity = Submission.order("created_at DESC").limit(10).includes(:user)
-      submission_activity.each do |s|
-        activity_hash = { message: "#{s.user.username} submitted their art.", datetime: s.created_at, link: submission_path(s.id) }
-        @activity.push(activity_hash)
-      end
-      comment_activity = Comment.order("created_at DESC").limit(10).includes(:user)
-      comment_activity.each do |c|
-        if c.source_type == "Submission"
-          activity_hash = { message: "#{c.user.username} posted a comment on submission #{c.source_id}.", datetime: c.created_at, link: submission_path(c.source_id) }
-          @activity.push(activity_hash)
-        end
-      end
-      @activity = @activity.sort_by {|h| h[:datetime] }.reverse!
-      @activity = @activity[0..9]
+        @activeChallenges = Challenge.where('start_date <= ? AND (end_date > ? OR end_date IS NULL)', Date.current, Date.current).order("start_date ASC, end_date DESC")
+        @upcomingChallenges = Challenge.where('start_date > ?', Date.current).order("start_date ASC, end_date DESC")
+        @currentSeasonalChallenge = Challenge.where(":todays_date >= start_date AND :todays_date < end_date AND seasonal = true", {todays_date: Date.current}).first
+        
+        # All lists to be displayed in home
+        @latestAwards = Award.where("date_received = ? AND badge_id <> 1", Date.today).order("prestige DESC, badge_id DESC").includes(:user)
+        @levelUps = Award.where("date_received = ? AND badge_id = 1", Date.today).order("prestige DESC").includes(:user)
+        @latestEliminations = Participation.where("challenge_id = 1 AND eliminated AND end_date = ?", (Date.current - 1.day)).order("score DESC").includes(:user)
+        @startingChallenges = @activeChallenges.where('start_date = ?', Date.current)
+        @endingChallenges = Challenge.where('end_date = ?', Date.current)
+        @seasonalLeaderboard = Participation.where("challenge_id = ?", @currentSeasonalChallenge.id).order("score DESC").includes(:user)
     end
     
     def login
     end
     
     def news
-      @latestPatchNote = PatchNote.last
-      @latestPatchEntries = PatchEntry.where('patchnote_id = ?', @latestPatchNote.id).order("importance DESC")
+        @latestPatchNote = PatchNote.last
+        @latestPatchEntries = PatchEntry.where('patchnote_id = ?', @latestPatchNote.id).order("importance DESC")
     end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -14,12 +14,6 @@ class Submission < ApplicationRecord
     validates :nsfw_level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
     validate :image_size_validation
  
-    def is_animated_gif
-      image = MiniMagick::Image.open(self.drawing.url)
-      return false if image.type.downcase != "gif"
-      return image.layers.count > 1
-    end
- 
     private
       def image_size_validation
         errors[:drawing] << "should be less than 10.0 MB" if drawing.size > 10.0 .megabytes
