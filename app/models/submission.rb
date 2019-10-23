90fca4ed057e5041352798467fa46542fe48db7a
EC2 Default User <ec2-user@ip-172-31-42-212.us-east-2.compute.internal>
2019-10-23 02:14:41 +0000

    Increase text limits.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -8,8 +8,8 @@ class Submission < ApplicationRecord
     
     validates :user_id, presence: true
     validates :drawing, presence: true
-    validates :title, length: { maximum: 50 }
-    validates :description, length: { maximum: 1000 }
+    validates :title, length: { maximum: 100 }
+    validates :description, length: { maximum: 2000 }
     #validates :thumbnail, presence: true
     validates :nsfw_level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
     validate :image_size_validation
