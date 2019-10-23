e20f30d56efa34cab7867401ab448c8063ba9f95
the1banana <the1banana@users.noreply.github.com>
2019-10-22 22:23:24 -0500

    Update 20191023014113_add_num_comments_to_submission.rb

--- db/migrate/20191023014113_add_num_comments_to_submission.rb
+++ db/migrate/20191023014113_add_num_comments_to_submission.rb
@@ -1,5 +1,5 @@
 class AddNumCommentsToSubmission < ActiveRecord::Migration[5.0]
   def change
-    add_column :submissions, :num_comments, :integer
+    add_column :submissions, :num_comments, :integer, null: false, default: 0
   end
 end
