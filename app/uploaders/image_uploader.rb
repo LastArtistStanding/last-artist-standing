e6b123119ce1103cf3ff9cf077e57aec69c08c0b
EC2 Default User <ec2-user@ip-172-31-42-212.us-east-2.compute.internal>
2019-10-26 05:50:05 +0000

    fix gif case... again

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,7 +12,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   process :resize_to_limit => [3000, 3000], if: :requires_resize?
   process :strip_exif
-  process :check_is_animated_gif
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -38,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :remove_animation
+    process :remove_animation, if: :is_animated_gif?
     process :resize_to_fill => [400, 400]
   end
   
@@ -63,9 +62,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   protected
   def remove_animation
-    if content_type == 'image/gif'
-      manipulate! { |image| image.collapse! }
-    end
+    puts "Removing animation!"
+    manipulate! { |image| image.collapse! }
   end
   
   protected
@@ -80,16 +78,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   
   protected
-  def check_is_animated_gif
-    if model.class.name == "Submission" && content_type == 'image/gif'
-      if file
-        examining_image = ::MiniMagick::Image.open(file.file)
-        if examining_image.layers.count > 1
-          model.is_animated_gif = true
-          model.save
-        end
-      end
+  def is_animated_gif?(file)
+    puts "Try to check gif!"
+    animated = false
+    if content_type == 'image/gif' && file
+      puts "Whoa, it's a gif!"
+      examining_image = ::MiniMagick::Image.open(file.file)
+      animated = true if examining_image.layers.count > 1
     end
+    puts "And is it animated? #{animated}"
+    if model.class.name == "Submission"
+      model.is_animated_gif = animated
+      model.save
+    end
+    animated
   end
   
   protected
