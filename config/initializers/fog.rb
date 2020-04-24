1296393db57eafa8267ed421a4c0cc1826b18893
James Martin <james@jtmar.me>
2020-04-23 18:20:44 -0700

    Make the AWS bucket region configurable with the AWS_REGION env variable.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
         provider:               'AWS',
         aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-        region:                 'us-east-2',
+        region:                 ENV['AWS_REGION'] || 'us-east-2',
     }
     config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_public = true
