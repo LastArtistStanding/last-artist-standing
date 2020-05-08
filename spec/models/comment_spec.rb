5feb70416075b8fabe11ffa7a4f03043202b02d4
James Martin <james@jtmar.me>
2020-05-08 15:08:49 -0700

    Fixed rspec tests to the extent currently possible pending #82.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe Comment do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/patch_entry_spec.rb
+++ spec/models/patch_entry_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe PatchEntry do
   pending "add some examples to (or delete) #{__FILE__}"
 end
