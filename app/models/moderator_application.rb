7fdb1de945421d4675fd3cf63cd5fc29a39c73ae
James Martin <james@jtmar.me>
2020-05-18 21:06:39 -0700

    Un-break moderator application form error messages.

--- app/models/moderator_application.rb
+++ app/models/moderator_application.rb
@@ -16,7 +16,7 @@ class ModeratorApplication < ApplicationRecord
   # The user must confirm that they are at least 18 years old.
   # This data is not stored in the database, but the box must be checked on the form.
   validates :at_least_18_years_old,
-            acceptance: 'You must be at least 18 years old to apply to be a moderator.'
+            acceptance: { message: 'You must be at least 18 years old to apply to be a moderator.' }
 
   # The user's time zone, in Ruby's TimeZone format.
   validates :time_zone, presence: 'You must specify your time zone.'
@@ -27,25 +27,29 @@ class ModeratorApplication < ApplicationRecord
   end
 
   # The hours that the user would be available to actively moderate.
-  validates :active_hours, length: LENGTH_RESTRICTION,
-                           presence: 'You must specify when you can actively moderate the site.'
-
+  validates :active_hours,
+            length: LENGTH_RESTRICTION,
+            presence: { message: 'You must specify when you can actively moderate the site.' }
 
   # Why the user wants to be a moderator.
-  validates :why_mod, length: LENGTH_RESTRICTION,
-                      presence: 'You must explain why you should become a moderator.'
+  validates :why_mod,
+            length: LENGTH_RESTRICTION,
+            presence: { message: 'You must explain why you should become a moderator.' }
 
   # The user's past experience as a moderator.
-  validates :past_experience, length: LENGTH_RESTRICTION,
-                              presence: 'You state whether you have any past experience as a moderator.'
+  validates :past_experience,
+            length: LENGTH_RESTRICTION,
+            presence: { message: 'You state whether you have any past experience as a moderator.' }
 
   # How long has the user been a member of LAS/DED/DAD.
-  validates :how_long, length: LENGTH_RESTRICTION,
-                       presence: 'You must state how long you have been a member of DAD.'
+  validates :how_long,
+            length: LENGTH_RESTRICTION,
+            presence: { message: 'You must state how long you have been a member of DAD.' }
 
   # What does dad mean to you?
-  validates :why_dad, length: LENGTH_RESTRICTION,
-                      presence: 'You must explain what DAD means to you.'
+  validates :why_dad,
+            length: LENGTH_RESTRICTION,
+            presence: { message: 'You must explain what DAD means to you.' }
 
   # Anything else the user would like to include in their application.
   validates :anything_else, length: LENGTH_RESTRICTION
