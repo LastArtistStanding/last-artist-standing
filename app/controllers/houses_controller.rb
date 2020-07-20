15861527bb14c360b43d3e76d52c25bc58a6fdb6
nsix <nsix@cock.li>
2020-07-20 00:38:58 -0700

    more rubocop fixes

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,13 +5,13 @@ class HousesController < ApplicationController
   def index
     @user_list = {}
     # TODO: Users.where('house_id is not null').each do |user|
-    User.where('1 = 1').each do |user|
-      @user_list[user.name] = 0
-      Submission.where('user_id = ? and created_at >= ?', user.id, Date.today - 30.days).each do |s|
-        @user_list[user.name] += s.time.to_f / 60.0
+    User.where("1 = 1").each do |user|
+      @user_list[user[:name]] = 0
+      Submission.where("user_id = ? and created_at >= ?", user[:id], Date.today - 30.days).each do |s|
+        @user_list[user[:name]] += s[:time].to_f / 60.0
       end
     end
     @total_time = @user_list.values.sum
-    @user_list = @user_list.delete_if{|k, v| !v.positive?}.sort_by { |hash| -hash.last }
+    @user_list = @user_list.delete_if { |_k, v| !v.positive? }.sort_by { |hash| -hash.last }
   end
 end
--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -1,32 +1,6 @@
 # frozen_string_literal: true
 
 describe HousesController do
-
-  class MockUser
-    def initialize id
-      @name = (id).to_s
-      @id = id
-    end
-
-    def id
-      @id
-    end
-
-    def name
-      @name
-    end
-  end
-
-  class MockSubmission
-    def initialize time
-      @time = time
-    end
-
-    def time
-      @time
-    end
-  end
-
   describe 'GET :index' do
     it 'renders the houses page' do
       get :index
@@ -35,18 +9,11 @@ describe HousesController do
 
     it 'populates the user_list variable' do
       houses = HousesController.new
-      allow(User).to receive(:where).and_return([MockUser.new(1), MockUser.new(2)])
-      allow(Submission).to receive(:where).and_return([MockSubmission.new(0)], [MockSubmission.new(120)])
-      # User.stub(:find_each).and_yield(test_users[0]).and_yield(test_users[1])
-      # Submission.stub(:where).and_yield(test_submissions)
+      allow(User).to receive(:where).and_return([{name: "1", id: 1}, {name: "2", id: 2}])
+      allow(Submission).to receive(:where).and_return([{time: 0}], [{time: 120}])
       houses.index
-      user_list = houses.instance_variable_get(:@user_list)
-      total_time = houses.instance_variable_get(:@total_time)
-      p user_list.length
-      expect(user_list.length).to eq(1)
-      expect(total_time).to eq(2)
+      expect(houses.instance_variable_get(:@user_list).length).to eq(1)
+      expect(houses.instance_variable_get(:@total_time)).to eq(2)
     end
   end
-
-
 end
