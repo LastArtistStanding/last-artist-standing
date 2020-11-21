f3dc8849a4bddc1726a8f24cb4b44e3bad390f7b
nsix <nsix@cock.li>
2020-11-20 22:03:05 -0800

    functionality done. styling tbd

--- app/assets/javascripts/submissions/view_size_change.js
+++ app/assets/javascripts/submissions/view_size_change.js
@@ -1,5 +1,8 @@
 var small = true;
 window.onload = function(e){
+  if (window.location.toString().match(/submissions\/\d+$/) === null) {
+    return
+  }
   document.getElementById("submissionImage").onclick = function(e) {
     if (small) {
       e.target.setAttribute("style", "cursor: zoom-out;")
