29cb59c947b625ced67568865352fb4366c73887
James Martin <james@jtmar.me>
2020-12-27 16:34:46 -0800

    Fix view size change regex to work on URLs with fragments.

--- app/assets/javascripts/submissions/view_size_change.js
+++ app/assets/javascripts/submissions/view_size_change.js
@@ -1,14 +1,14 @@
 var small = true;
-window.onload = function(e){
-  if (window.location.toString().match(/submissions\/\d+$/) === null) {
-    return
+window.onload = function(e) {
+  if (window.location.toString().match(/submissions\/\d+(\#.*)?$/) === null) {
+    return;
   }
   document.getElementById("submissionImage").onclick = function(e) {
     if (small) {
-      e.target.setAttribute("style", "cursor: zoom-out;")
+      e.target.setAttribute("style", "cursor: zoom-out;");
     } else {
       e.target.setAttribute("style", "max-width: 100%; max-height: 600px; cursor: zoom-in;");
     }
     small = !small;
-  }
+  };
 };
