ec7d3ba86f7b1ae55706504f26093443fd884b84
nsix <36219436+nsixcancode@users.noreply.github.com>
2021-01-10 15:18:38 -0800

    pulled in fixes onto new branch (#147)
    Co-authored-by: nsix <nsix@cock.li>

--- spec/controllers/followers_controller_spec.rb
+++ spec/controllers/followers_controller_spec.rb
@@ -17,27 +17,21 @@ describe FollowersController do
     users = []
   end
 
-  describe 'GET :follow' do
+  describe 'POST :follow' do
     it 'creates a follower entry in the DB' do
-      allow(Follower).to receive(:create).and_return(true)
-      get :follow, params: { id: 1 }
-      expect(response).to redirect_to root_url
-    end
-
-    it 'does not create duplicate follows' do
-      allow(Follower).to receive(:where).and_return({ empty?: false })
-      get :follow, params: { id: 1 }
-      expect(Follower).not_to receive(:create)
+      allow(controller).to receive(:ensure_authenticated).and_return(true)
+      allow(controller).to receive(:current_user).and_return(users[0])
+      post :follow, params: { id: users[1].id }
+      expect(response).to redirect_to user_path(users[1])
     end
   end
 
-  describe 'GET :unfollow' do
+  describe 'POST :unfollow' do
     it 'deletes a follower entry from the db' do
-      follower = Follower.create({ user: users[0], following: users[1] })
+      Follower.create({ user: users[0], following: users[1] })
       allow(controller).to receive(:current_user).and_return(users[0])
-      get :unfollow, params: { id: users[1].id }
-      expect(follower).to receive(:destroy)
-      follower.destroy
+      post :unfollow, params: { id: users[1].id }
+      expect(Follower.where({ user_id: users[0], following: users[1] })).to be_empty
     end
   end
 end
--- spec/requests/houses_request_spec.rb
+++ spec/requests/houses_request_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe "Houses", type: :request do
-
-  describe "GET /houses" do
-    it "returns http success" do
-      get "/houses"
+RSpec.describe 'Houses', type: :request do
+  describe 'GET /houses' do
+    it 'returns http success' do
+      get '/houses'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
