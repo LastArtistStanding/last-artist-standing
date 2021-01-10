f29676f400808d679513bded2645833d597f2781
nsix <nsix@cock.li>
2021-01-10 14:18:35 -0800

    pulled in fixes onto new branch

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
 # Controller for handling followers
-class FollowersController < ActionController::Base
+class FollowersController < ApplicationController
+  before_action :ensure_authenticated, only: %i[follow unfollow]
   include SessionsHelper
 
   def follow
-    if current_user&.id != params[:id]
-      Follower
-        .where({ user_id: current_user&.id, following: User.find(params[:id]) })
-        .first_or_create
-    end
-    redirect_back(fallback_location: root_path)
+    Follower
+      .where({ user_id: current_user.id, following: User.find(params[:id]) })
+      .first_or_create
+    redirect_back(fallback_location:  user_path(params[:id]))
   end
 
   def unfollow
-    Follower.where({ user_id: current_user&.id, following: User.find(params[:id]) })
+    Follower.where({ user_id: current_user.id, following: User.find(params[:id]) })
         &.first
         &.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: user_path(params[:id]))
   end
 end
