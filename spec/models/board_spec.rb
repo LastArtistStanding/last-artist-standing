577ce00ab8b73d1279b663325a576a2422aa34e3
the1banana <theonebanana@gmail.com>
2021-08-25 00:31:36 -0400

    Test force fail build.

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ':test_failure' do
+    it 'the dragon is burning down your build!' do
+      expect("your code").to be("fucked")
+    end
+  end
 end
