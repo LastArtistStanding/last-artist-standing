865ab8a7301f75502a2adabf7c9f23a1e5c70618
the1banana <theonebanana@gmail.com>
2023-07-24 05:23:27 -0400

    permit YAML classes

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    config.active_record.yaml_column_permitted_classes += [Matrix, OpenStruct, Symbol]
   end
 end
