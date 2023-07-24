ac7cbbdbb76733a2977c52dfa67fbe9a0cba9b4d
the1banana <theonebanana@gmail.com>
2023-07-24 05:14:55 -0400

    fix breaking migration change

--- db/migrate/20180327035035_add_reset_to_users.rb
+++ db/migrate/20180327035035_add_reset_to_users.rb
@@ -3,7 +3,7 @@
 # Adds reset_digest and reset_sent_at columns to users
 class AddResetToUsers < ActiveRecord::Migration[5.0]
   def change
-    change_table :users, { bulk: true } do |t|
+    change_table :users do |t|
       t.string :reset_digest
       t.datetime :reset_sent_at
     end
